48e32e4205a7c0ab748ba59391b7f67c9bf465e9
Felix Jung <felix@jung.fm>
2014-11-27 19:55:34 +0100

    Basic podcast section working

--- _src/js/itunespodcasts.js
+++ _src/js/itunespodcasts.js
@@ -1,6 +1,11 @@
 function processPodcast(data) {
   var iTunesResult = data.results[0]
-  var htmlOutput   = '<img class = "podcast-cover" src="' + iTunesResult.artworkUrl600 + '"/>'
+  var htmlOutput   = ['<div class="podcast">',
+    '  <a href="' + iTunesResult.collectionViewUrl + '" target="_blank">',
+    '    <img class = "podcast-cover" src="' + iTunesResult.artworkUrl600 + '"/>',
+    '  </a>',
+    '</div>'
+  ]
 
-  document.write(htmlOutput)
+  document.write(htmlOutput.join('\n'))
 }
