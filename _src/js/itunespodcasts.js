5037a1c1808fd6f627125d7e852ed57c2288c79b
Felix Jung <felix@jung.fm>
2014-12-08 22:20:27 +0100

    Introduce new jquery to run script on document ready

--- _src/js/itunespodcasts.js
+++ _src/js/itunespodcasts.js
@@ -1,11 +1,29 @@
 function processPodcast(data) {
-  var iTunesResult = data.results[0]
+  var targetDiv    = $('#podcasts').children().children();
+  var iTunesResult = data.results[0];
   var htmlOutput   = ['<div class="podcast">',
     '  <a href="' + iTunesResult.collectionViewUrl + '" target="_blank">',
     '    <img class = "podcast-cover" src="' + iTunesResult.artworkUrl600 + '"/>',
     '  </a>',
     '</div>'
-  ]
+  ];
 
-  document.write(htmlOutput.join('\n'))
-}
+  targetDiv.append(htmlOutput.join('\n'));
+};
+
+$(document).ready(function() {
+  // Search terms
+  var apiCalls = [
+    'https://itunes.apple.com/search?term=accidental+tech+podcast&entity=podcast&callback=?',
+    'https://itunes.apple.com/search?term=the+talk+show+gruber&artist=gruber&entity=podcast&callback=?',
+    'https://itunes.apple.com/search?term=debug+ritchie+guy+english&entity=podcast&callback=?',
+    'https://itunes.apple.com/search?term=freak+show+metaebene&entity=podcast&callback=?',
+    'https://itunes.apple.com/search?term=the+record+brent+simmons&entity=podcast&callback=?',
+    'https://itunes.apple.com/search?term=iterate+ritchie+edwards&entity=podcast&callback=?',
+    'https://itunes.apple.com/search?term=core+intuition+manton&entity=podcast&callback=?'
+  ];
+
+  apiCalls.forEach(function(apiCall) {
+    $.getJSON(apiCall, processPodcast);
+  });
+});
