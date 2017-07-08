36b4597c8955a3f51f132c60a69993eb2bbb6bbd
Felix Jung <felix@jung.fm>
2017-07-08 16:49:35 +0200

    Change site title.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: 'felixjung.io',
   },
-  plugins: [`gatsby-plugin-react-helmet`],
+  plugins: ['gatsby-plugin-react-helmet'],
 }
