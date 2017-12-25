b145be2c72d9f7b174d27d39231f06b1572e9a87
Felix Jung <felix@jung.fm>
2017-12-25 15:27:05 +0100

    Adjust font size

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,6 +3,7 @@ import styled from 'react-emotion'
 import { mainContainer } from '../styles/layout-styles'
 
 const Footer = styled('footer')(...mainContainer, ({ theme }) => ({
+  fontSize: theme.fontSize.xxs,
   marginTop: `calc(2 * ${theme.spacing.xxxl})`,
   marginBottom: `calc(${theme.spacing.xxxl})`
 }))
