18000f0f6e5838b2d9e994b31673f82f43f442ba
Felix Jung <felix@jung.fm>
2018-01-20 13:27:27 +0100

    Fix sitemap generation

--- gatsby-config.js
+++ gatsby-config.js
@@ -77,40 +77,7 @@ module.exports = {
       }
     },
     {
-      resolve: `gatsby-plugin-sitemap`,
-      options: {
-        output: `/sitemap.xml`,
-        query: `
-        {
-          site {
-            siteMetadata {
-              siteUrl
-            }
-          }
-
-          allSitePage(
-            filter: {
-              path: {
-                regex: "/^(?!/(dev-404-page|404|)).*$/"
-              }
-            }
-          ) {
-            edges {
-              node {
-                path
-              }
-            }
-          }
-        }`,
-        serialize: ({ site, allSitePage }) =>
-          allSitePage.edges.map(edge => {
-            return {
-              url: site.siteMetadata.siteUrl + edge.node.path,
-              changefreq: `daily`,
-              priority: 0.7
-            }
-          })
-      }
+      resolve: 'gatsby-plugin-sitemap'
     }
   ]
 }
